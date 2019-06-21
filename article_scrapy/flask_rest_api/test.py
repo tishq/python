41cba97693c2564ad3289bfcc592b83a788e9e37
tishq <737499655@qq.com>
2019-06-21 20:16:44 +0800

    commit

--- article_scrapy/flask_rest_api/test.py
+++ article_scrapy/flask_rest_api/test.py
@@ -1,16 +1,31 @@
-import math
+# flask restfull
+from flask import Flask
+from flask_restful import reqparse, abort, Api, Resource
 
 
-def UserSimilarity(train):
-    W = dict()
-    for u in train.keys():
-        for v in train.keys():
-            if u == v:
-                continue
-        W[u][v] = len(train[u] & train[v])
-        W[u][v] /= math.sqrt(len(train[u]) * len(train[v]) * 1.0)
-    return W
+
+app = Flask(__name__)
+api = Api(app)
+
+
+
+
+
+
+class ArticlesR(Resource):
+    def get(self,userId):
+        # userId = int(parser.parse_args()['userId'])
+        print(userId)
+        print(type(userId))
+
+        return userId
+
+api.add_resource(ArticlesR, '/articles/<userId>')
+
 if __name__ == '__main__':
-    train = {'A': {'a','b','c'},'B': {'a','b','c','d'}}
-    aa = UserSimilarity(train)
-    print(aa)
+    app.run(
+        # python es_rest.py 可以看到效果(生产环境)
+        host= '0.0.0.0',
+        port= 5009,
+        debug=True
+    )
